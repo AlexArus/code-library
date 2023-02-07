728320c537f42838599a8f7dd6e5152980583329
Alex Arus <mr.alex.arus@gmail.com>
2023-02-07 10:59:30 +0000

    [doc] add array jsdoc

--- types/array.ts
+++ types/array.ts
@@ -1,5 +1,7 @@
+/** Get type of array elements (accept only arrays) */
 export type ArrayElement<ArrayType extends readonly unknown[]> =
-    ArrayType extends readonly (infer ElementType)[] ? ElementType : never;
+  ArrayType extends readonly (infer ElementType)[] ? ElementType : never;
 
+/** Get type of array elements (accept any object, if object is not array return never) */
 export type ArrayElementOfAny<ArrayType> =
-    ArrayType extends readonly (infer ElementType)[] ? ElementType : never;
+  ArrayType extends readonly (infer ElementType)[] ? ElementType : never;
