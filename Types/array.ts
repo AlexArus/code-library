d1a906e8c4a5358b0780e8d072beda626a2fe9a1
Alexander Selivanov <mr.alex.arus@gmail.com>
2024-04-08 06:34:53 +0000

    update array element type

--- Types/array.ts
+++ Types/array.ts
@@ -1,3 +1,6 @@
+/** Get type of array elements (accept any object, doesn't work with readonly arrays) */
+export type ArrayElementSimple<T> = T extends Array<infer V> ? V : never;
+
 /** Get type of array elements (accept only arrays) */
 export type ArrayElement<ArrayType extends readonly unknown[]> =
   ArrayType extends readonly (infer ElementType)[] ? ElementType : never;
